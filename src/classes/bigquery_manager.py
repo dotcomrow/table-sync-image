c608fe0ff5f6686e09a9317df843144b2381d4fc
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-10-11 16:06:11 -0400

    cleanup
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/classes/bigquery_manager.py
+++ src/classes/bigquery_manager.py
@@ -53,53 +53,6 @@ class BigQueryManager:
             self.client.create_dataset(dataset)
             self.logger.info("Dataset created successfully", dataset_id=dataset_id)
 
-    def create_table(self, table_info, schema):
-        self._initialize_client()
-        self.logger.info("Creating table in BigQuery", table_info=table_info)
-        dataset_id = table_info.bq_dataset
-        table_id = table_info.bq_table
-        if not dataset_id or not table_id:
-            self.logger.error("Invalid dataset or table ID", dataset_id=dataset_id, table_id=table_id)
-            raise ValueError("Invalid dataset or table ID")
-
-        # Check if dataset exists
-        dataset_ref = self.client.dataset(dataset_id)
-        try:
-            self.logger.debug("Checking if dataset exists", dataset_id=dataset_id)
-            self.client.get_dataset(dataset_ref)
-            self.logger.info("Dataset exists", dataset_id=dataset_id)
-        except Exception as e:
-            self.logger.warning("Dataset does not exist, creating it", dataset_id=dataset_id, error=str(e))
-            dataset = bigquery.Dataset(dataset_ref)
-            dataset.location = "US"  # Set location or make it configurable
-            self.logger.debug("Creating dataset", dataset_id=dataset_id, location=dataset.location)
-            self.client.create_dataset(dataset)
-            self.logger.info("Dataset created successfully", dataset_id=dataset_id)
-
-        # Log schema conversion details
-        self.logger.debug("Starting schema conversion", input_schema=schema)
-        try:
-            converted_schema = [
-                bigquery.SchemaField(field.name, field.field_type, mode=field.mode)
-                for field in schema
-            ]
-            self.logger.info("Schema conversion successful", converted_schema=converted_schema)
-        except Exception as e:
-            self.logger.error("Schema conversion failed", error=str(e))
-            raise
-
-        # Log table creation details
-        table_ref = dataset_ref.table(table_id)
-        self.logger.debug("Preparing to create table", table_ref=str(table_ref), schema=converted_schema)
-        table = bigquery.Table(table_ref, schema=converted_schema)
-        try:
-            self.logger.debug("Sending request to create table", table_ref=str(table_ref))
-            response = self.client.create_table(table)
-            self.logger.info("Table created successfully", table=table_info, response=str(response))
-        except Exception as e:
-            self.logger.error("Failed to create table", table_ref=str(table_ref), error=str(e))
-            raise
-
     def delete_table(self, table_info: TableInfo):
         self._initialize_client()
         self.logger.info("Deleting table in BigQuery", dataset_id=table_info.bq_dataset, table_id=table_info.bq_table)
