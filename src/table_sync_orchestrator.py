54f6884536cabd54f9c9916eb54829612676d386
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2025-10-20 20:19:49 -0400

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- src/table_sync_orchestrator.py
+++ src/table_sync_orchestrator.py
@@ -345,6 +345,7 @@ class TableSyncOrchestrator:
                     if bigquery_manager.check_table_exists(table_info.bq_dataset, table_info.bq_table):
                         logger.logMessage(Logging.LogLevel.DEBUG, "BigQuery table exists for annotated table, backfilling data into yugabyte", table=table_info.to_dict(), thread=threading.current_thread().name)
                         data = bigquery_manager.fetch_bigquery_data(table_info)
+                        yugabyte_manager.clear_yugabyte_table(table_info)
                         yugabyte_manager.insert_into_yugabyte(data, table_info)
                     
                     connector_statuses = kafka_connector.check_connector_exists(table_info)
